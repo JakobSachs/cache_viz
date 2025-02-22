aa5a968729c80fcb1c941ee78d10b7f28c4ef6c1
Jakob Sachs <jakobsachs1999@gmail.com>
2025-02-23 00:13:55 +0100

    feat: Add shared statistics tracking to Core cache operations

--- backend/src/core.rs
+++ backend/src/core.rs
@@ -16,6 +16,7 @@ pub struct CoreStats {
 pub struct Core {
     private_cache: Cache,
     shared_cache: Arc<Mutex<Cache>>,
+    shared_stats: Arc<Mutex<CoreStats>>,
     pub stats: CoreStats,
 }
 
@@ -24,28 +25,51 @@ impl Core {
         cache_size: NonZeroU32,
         cache_associativity: NonZeroU32,
         shared_cache: Arc<Mutex<Cache>>,
+        shared_stats: Arc<Mutex<CoreStats>>,
     ) -> Result<Self, ()> {
         let c = Cache::new(cache_size, cache_associativity)?;
         Ok(Core {
             stats: CoreStats::default(),
             private_cache: c,
             shared_cache,
+            shared_stats,
         })
     }
 
-    pub fn read(&mut self, addr: u32) -> bool {
-        match self.private_cache.read(addr) {
+    pub fn read(&mut self, addr: u32) {
+        let local_res = self.private_cache.read(addr);
+        match local_res {
             true => self.stats.read_hit += 1,
             false => self.stats.read_miss += 1,
         }
-        true
+        if !local_res {
+            let mut s_c = self.shared_cache.lock().unwrap();
+            let mut s_t = self.shared_stats.lock().unwrap();
+            match s_c.read(addr) {
+                true => s_t.read_hit += 1,
+                false => {
+                    s_t.read_miss += 1;
+                }
+            }
+        }
     }
 
-    pub fn write(&mut self, addr: u32, val: u32) -> bool {
-        match self.private_cache.write(addr, val) {
+    pub fn write(&mut self, addr: u32, val: u32) {
+        let local_res = self.private_cache.write(addr, val);
+        match local_res {
             true => self.stats.write_hit += 1,
             false => self.stats.write_miss += 1,
         }
-        true
+        // if we have a local miss, we go to shared
+        if !local_res {
+            let mut s_c = self.shared_cache.lock().unwrap();
+            let mut s_t = self.shared_stats.lock().unwrap();
+            match s_c.write(addr, val) {
+                true => s_t.write_hit += 1,
+                false => {
+                    s_t.write_miss += 1;
+                }
+            }
+        }
     }
 }
