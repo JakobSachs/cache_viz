7dfc5aa66318a3ca34f305d17937062404269cc2
Jakob Sachs <jakobsachs1999@gmail.com>
2025-02-22 23:48:12 +0100

    feat: Add initial backend library file in src directory

---
+++ backend/src/lib.rs
@@ -0,0 +1,61 @@
+mod cache;
+mod core;
+
+use std::borrow::BorrowMut;
+use std::num::NonZeroU32;
+use std::sync::{Arc, Mutex};
+
+use rand::Rng;
+
+use crate::cache::Cache;
+use crate::core::Core;
+
+#[derive(Default)]
+struct Stats {}
+
+struct Simulation {
+    cores: Vec<Core>,
+    shared_cache: Arc<Mutex<Cache>>,
+    cycle: u32,
+    stats: Stats,
+}
+
+impl Simulation {
+    pub fn new(
+        num_cores: NonZeroU32,
+        l1_cache_size: NonZeroU32,
+        l1_cache_associativity: NonZeroU32,
+        l2_cache_size: NonZeroU32,
+        l2_cache_associativity: NonZeroU32,
+    ) -> Result<Self, ()> {
+        let shared_cache = Arc::new(Mutex::new(Cache::new(
+            l2_cache_size,
+            l2_cache_associativity,
+        )?));
+
+        let cores: Vec<Core> = (0..num_cores.get())
+            .map(|_| Core::new(l1_cache_size, l1_cache_associativity, shared_cache.clone()))
+            .collect::<Result<_, ()>>()?;
+
+        Ok(Simulation {
+            cores,
+            shared_cache,
+            cycle: 0,
+            stats: Stats::default(),
+        })
+    }
+
+    pub fn step(&mut self) {
+        let mut rng = rand::rng();
+
+        for core in &mut self.cores {
+            let addr: u32 = rng.random::<u32>() >> 24;
+            let val: u32 = rng.random();
+            let is_write: bool = rng.random();
+            let res = match is_write {
+                true => core.write(addr, val),
+                false => core.read(addr),
+            };
+        }
+    }
+}
